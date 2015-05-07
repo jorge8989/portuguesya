a65e243bcf059a7f9ff2435329511da01b625ed6
jorgearteaga <jorge_arteaga89@hotmail.com>
2015-05-07 00:24:20 -0430

    init

---
+++ app/assets/javascripts/capitais.js
@@ -0,0 +1,66 @@
+var estados = ['do Rio grande do sul', 'de Santa Catarina', 'do Paraná', 'do São Paulo', 'do Rio de janeiro', 'do Espíritu santo', 'de Minas gerais', 'de Goias', 'do Mato grosso', 'do Mato Grosso do sul', 'de Rondônia', 'do Acre', 'de Amazonas', 'do Pará', 'de Roraima', 'de Amapá', 'de Tocantins', 'do Piauí', 'do Maranhão', 'do Ceará', 'do Rio grande do norte', 'da Paraiba', 'de Alagoas', 'de Pernambuco', 'de Sergipe', 'da Bahia']
+var capitais = ['Porto alegre', 'Florianópolis', 'Curitiba', 'São Paulo', 'Rio de janeiro', 'Vitória', 'Belo horizonte', 'Goiánia', 'Cuiabá', 'Campo grande', 'Porto velho', 'Rio branco', 'Manaus', 'Belem', 'Boa vista', 'Macapá', 'Palmas', 'Teresina', 'São Luis', 'Fortaleza', 'Natal', 'João Pessoa', 'Maceió', 'Recife', 'Aracaju', 'Salvador' ];
+var resposta = ''
+var resjugador = ''
+var randest = ''
+var opciones = []
+var bem = 0
+var mal = 0
+function gpregunta() {
+var rne = Math.round(Math.random()*25);
+randest = estados[rne];
+document.getElementById("pregunta").innerHTML=randest;
+resposta = capitais[rne];
+}
+function jugar() {
+document.getElementById("acertos").style.visibility="visible";
+document.getElementById("acertosp").innerHTML=bem
+document.getElementById("erros").style.visibility="visible";
+document.getElementById("errosp").innerHTML=mal
+document.getElementById("qual").style.visibility="visible";
+document.getElementById("boton").style.display="none";
+document.getElementById("ul").style.visibility="visible"
+gpregunta();
+function generar(){
+var op1 = Math.round(Math.random()*25);
+var op2 = Math.round(Math.random()*25);
+for (op2 = op2 ; op1 == op2; op2 = (Math.round(Math.random()*25))) {
+op2 = op2
+}
+var op3 = Math.round(Math.random()*25);
+for (op3 = op3 ;( op3 == op2 || op3 == op1 ); op3 = (Math.round(Math.random()*25))) {
+op3 = op3
+}
+var op4 = Math.round(Math.random()*25);
+for (op4 = op4 ;( op4 == op3 || op4 == op2 || op4 == op1); op4 = (Math.round(Math.random()*25))) {
+op4 = op4
+}
+
+opciones = []
+opciones.push(capitais[op1], capitais[op2], capitais[op3], capitais[op4]);
+}
+generar();
+if ((opciones.indexOf(resposta)) < 0) {
+var rno = Math.round(Math.random()*3);
+opciones[rno] = resposta
+}
+document.getElementById("op1").innerHTML=opciones[0]
+document.getElementById("op2").innerHTML=opciones[1]
+document.getElementById("op3").innerHTML=opciones[2]
+document.getElementById("op4").innerHTML=opciones[3]
+}
+function analizar(elemento) {
+resjugador = elemento.innerHTML;
+if (resjugador == resposta) {
+document.getElementById("muito").innerHTML="Muito Bem!!!"
+bem += 1
+}
+else {
+document.getElementById("muito").innerHTML="Muito Mal!!!"
+mal += 1
+}
+document.getElementById("res").style.visibility="visible";
+document.getElementById("resposta").innerHTML=resposta;
+document.getElementById("ja").style.visibility="visible";
+jugar();
+}
